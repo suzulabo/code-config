d7300cc9d10d313eb204c60b221513e818d5278e
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-12 00:35:58 +0900

    Add CI

--- test.cjs
+++ test.cjs
@@ -6,6 +6,14 @@ const cmd = (s) => {
   return cmd.status;
 };
 
+if (cmd('pnpm exec eslint .') != 0) {
+  exit(1);
+}
+
+if (cmd('pnpm exec prettier --check .') != 0) {
+  exit(1);
+}
+
 if (cmd('pnpm exec tsc') == 0) {
   exit(1);
 }
